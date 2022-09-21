b004b2ec3d26e8f21e224d56f34c7d700a06ab2e
mateusadd <mateus.add02@gmail.com>
2022-09-20 21:04:41 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# y = w1*x1 + w2*x2 + w3*x3 + w4*x4 + w5*x5 + w6*x6
+
+import numpy as np
+
+def main():
+    #variáveis x1
+    equation_inputs = [4, -2, 3.5, 5, -11, -4.7]
+    print('hello')
+
+#se executar o script, chama main()
+if __name__ == '__main__':
+    main()
